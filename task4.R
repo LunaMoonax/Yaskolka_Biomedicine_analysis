9f6b53855b90cc41d2144d3ac249a14523087ae7
Viktorija Ramonaitė <viktorija.ramona@gmail.com>
2026-06-12 22:47:00 +0100

    Padarytas modelio mokymo etapas ir testavimas.

--- task4.R
+++ task4.R
@@ -92,6 +92,71 @@ cohort  <- full$cohort
 
 # full nebereikalingas kaip didelė matrica – atlaisvinam atmintį
 rm(full)
+# priverstinai atlaisviname atmintį
+gc()
+
+# Grąžina N CpG indeksų, labiausiai susijusių su amžiumi
+top_cpg <- function(rows, n_keep = 10000) {
+  cors <- as.vector(cor(age_log[rows], full_beta[rows, ]))
+  cors[is.na(cors)] <- 0
+  order(abs(cors), decreasing = TRUE)[seq_len(n_keep)]
+}
+
+# LOCO ciklas - leave-one-cohort-out
+# Testuojame ir mokome modelį - tai ne galutinis modelis.
+# Naudosime elastic-net modeli is bilbiotekos glmnet, pagal literatura tai turetu but tiksliausias.
+loco <- do.call(rbind, lapply(unique(cohort), function(test_c) {
+  train <- which(cohort != test_c)
+  test <- which(cohort == test_c)
+  
+  idx <- top_cpg(train, 10000)
+  
+  fit <- cv.glmnet(full_beta[train, idx], age_log[train],
+                   alpha = 0.5, type.measure = "mae", nfolds = 5)
+  
+  pred_log <- predict(fit, full_beta[test, idx], s = "lambda.min")
+  
+  out <- data.frame(
+    cohort = test_c,
+    true   = age_raw[test],
+    pred   = exp(as.vector(pred_log))
+  )
+  
+  rm(fit, idx); gc()
+  out
+}))
+
+# klaida metais
+loco$err <- loco$pred - loco$true
+
+# Bendras MAE (vidutinė absoliuti klaida metais)
+mae <- mean(abs(loco$err))
+cat("LOCO MAE (metais):", round(mae, 2), "\n")
+# LOCO MAE (metais): 3.51 
+
+# Klaida pagal rinkinį - ar koks nors iškrenta
+aggregate(abs(err) ~ cohort, data = loco, FUN = mean)
+#       cohort abs(err)
+#1       fraga 3.515834
+#2   johansson 3.700486
+#3 kalyakulina 4.409006
+#4   kurushima 3.392192
+#5       mahdi 3.443585
+#6          xu 2.598103
+
+# Pearson koreliacija tikras vs spėtas - kaip gerai seka
+cat("Pearson r:", round(cor(loco$true, loco$pred), 3), "\n")
+# Pearson r: 0.971 
 
 
+# Išvada kol kas:
+# Mokomas modelis jau labai tikslus pagal literatūra:
+# epigenetiniai laikrodziai kaip Horvath duoda MAE tarp 3-5 metu
+# koreliacija jei virs 0.95 - labai gera
 
+# Ka dar reikia padaryt:
+# Galima pabandyt dar pakoreguot modelio apmokyma su alpha
+# Siuo metu alfa parinktas random 0.5, bet derinant (mazinant ar aukstinant) butu galima
+# gal dar tikslesni rezultata gaut ir parodyt kaip buvo derinama
+# Pabaigti ir sukurti galutini modeli ant 5 duomenu rinkiniu, padaryti is jo funkcija ir istestuoti ant
+# palikto duomenu rinkinio quinn
