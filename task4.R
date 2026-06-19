2180807c3c6b09bb0ce01a2e7eb0adb64491a002
Viktorija Ramonaitė <viktorija.ramona@gmail.com>
2026-06-19 19:20:34 +0100

    Patikslinta funkcija ir išbandyta su didesniais cpg kiekiais.

--- task4.R
+++ task4.R
@@ -1,8 +1,8 @@
 # Ketvirta užduotis
 # Viktorija Ramonaitė, Skaistė Bartkutė
 
-#setwd("C:/Users/Viktorija Ramonaite/Desktop/UNIVERAS/3 KURSAS/BIOMEDICINOS DUOMENU ANALIZE/1 uzduotis/Yaskolka_Biomedicine_analysis")
-setwd("C:/Users/skais/Desktop/Universitetas/Šeštas semestras/Biomedicina/task1/Yaskolka_Biomedicine_analysis")
+setwd("C:/Users/Viktorija Ramonaite/Desktop/UNIVERAS/3 KURSAS/BIOMEDICINOS DUOMENU ANALIZE/1 uzduotis/Yaskolka_Biomedicine_analysis")
+#setwd("C:/Users/skais/Desktop/Universitetas/Šeštas semestras/Biomedicina/task1/Yaskolka_Biomedicine_analysis")
 
 library(annmatrix)
 library(dplyr) 
@@ -35,7 +35,7 @@ train_sets <- list(
   xu          = data_train_6
 )
 
-lapply(train_sets, function(x) colnames(colanns(x)))
+#lapply(train_sets, function(x) colnames(colanns(x)))
 
 # Surenkame tik bendrus CpG visiems duomenų rinkiniams
 common_cpg <- Reduce(intersect, lapply(train_sets, rownames))
@@ -97,7 +97,7 @@ rm(full)
 gc()
 
 # Grąžina N CpG indeksų, labiausiai susijusių su amžiumi
-top_cpg <- function(rows, n_keep = 10000) {
+top_cpg <- function(rows, n_keep = 30000) {
   cors <- as.vector(cor(age_log[rows], full_beta[rows, ]))
   cors[is.na(cors)] <- 0
   order(abs(cors), decreasing = TRUE)[seq_len(n_keep)]
@@ -111,7 +111,7 @@ loco <- do.call(rbind, lapply(unique(cohort), function(test_c) {
   train <- which(cohort != test_c)
   test <- which(cohort == test_c)
   
-  idx <- top_cpg(train, 10000)
+  idx <- top_cpg(train, 30000)
   
   fit <- cv.glmnet(full_beta[train, idx], age_log[train],
                    alpha = 0.5, type.measure = "mae", nfolds = 5)
@@ -134,21 +134,21 @@ loco$err <- loco$pred - loco$true
 # Bendras MAE (vidutinė absoliuti klaida metais)
 mae <- mean(abs(loco$err))
 cat("LOCO MAE (metais):", round(mae, 2), "\n")
-# LOCO MAE (metais): 3.51 
+# LOCO MAE (metais): 3.22 
 
 # Klaida pagal rinkinį - ar koks nors iškrenta
 aggregate(abs(err) ~ cohort, data = loco, FUN = mean)
 #       cohort abs(err)
-#1       fraga 3.515834
-#2   johansson 3.700486
-#3 kalyakulina 4.409006
-#4   kurushima 3.392192
-#5       mahdi 3.443585
-#6          xu 2.598103
+#1       fraga 3.417812
+#2   johansson 3.151418
+#3 kalyakulina 4.096869
+#4   kurushima 3.297235
+#5       mahdi 3.203025
+#6          xu 2.595903
 
 # Pearson koreliacija tikras vs spėtas - kaip gerai seka
 cat("Pearson r:", round(cor(loco$true, loco$pred), 3), "\n")
-# Pearson r: 0.971
+# Pearson r: 0.976
 
 # Ištestuojame alpha reikšmes loco ciklui intervale nuo 0.0 iki 1.0 (inkrementuojant kas 0.1)
 alphas <- seq(0, 1, by = 0.1)
@@ -160,7 +160,7 @@ diff_alphas <- do.call(rbind, lapply(alphas, function(alpha_val) {
     train <- which(cohort != test_c)
     test <- which(cohort == test_c)
   
-    idx <- top_cpg(train, 10000)
+    idx <- top_cpg(train, 30000)
   
     fit <- cv.glmnet(full_beta[train, idx], age_log[train],
                      alpha = alpha_val, type.measure = "mae", nfolds = 5)
@@ -189,24 +189,24 @@ diff_alphas <- do.call(rbind, lapply(alphas, function(alpha_val) {
 print(diff_alphas)
 
 # alpha      mae     corrs
-# 1    0.0 4.059213 0.9619025
-# 2    0.1 3.533190 0.9709141
-# 3    0.2 3.514222 0.9712781
-# 4    0.3 3.505293 0.9714638
-# 5    0.4 3.504516 0.9715078
-# 6    0.5 3.505156 0.9714920
-# 7    0.6 3.505789 0.9714905
-# 8    0.7 3.507992 0.9714620
-# 9    0.8 3.502974 0.9715427
-# 10   0.9 3.504274 0.9715227
-# 11   1.0 3.506883 0.9714905
+# 1    0.0 3.979830 0.9630074
+# 2    0.1 3.260759 0.9751381
+# 3    0.2 3.233772 0.9755313
+# 4    0.3 3.230032 0.9756267
+# 5    0.4 3.225269 0.9757220
+# 6    0.5 3.221066 0.9757852
+# 7    0.6 3.222216 0.9757986
+# 8    0.7 3.223162 0.9757962
+# 9    0.8 3.218841 0.9758506
+# 10   0.9 3.218243 0.9758670
+# 11   1.0 3.221888 0.9758339
 
 # Išsirenkame geriausią alpha - mažiausią pagal MAE ir didžiausią pagal koreliaciją.
 best_alpha <- diff_alphas[order(diff_alphas$mae, -diff_alphas$corrs), ][1, ]
 best_alpha
 
 # alpha      mae     corrs
-# 9   0.8 3.502974 0.9715427
+# 9   0.9 3.218243 0.975867
 
 # Sudarome pilną modelį
 
@@ -214,14 +214,14 @@ best_alpha
 cohort_num <- seq_along(cohort)
 
 # Atsirenkam top CpG remdamiesi visais duomenų rinkiniais.
-idx <- top_cpg(cohort_num, 10000)
+idx <- top_cpg(cohort_num, 30000)
 
 # Ieškom geriausio "lambda".
 fit <- cv.glmnet(full_beta[cohort_num, idx], age_log[cohort_num],
-                 alpha = 0.8)
+                 alpha = 0.9)
 
 # Sudarome galutinį modelį:
-best_fit <- glmnet(full_beta[cohort_num, idx], age_log[cohort_num], alpha = 0.8, lambda = fit$lambda.min)
+best_fit <- glmnet(full_beta[cohort_num, idx], age_log[cohort_num], alpha = 0.9, lambda = fit$lambda.min)
 
 # Kuriame funkciją, kuri paruoštų duomenis modelio pritaikymui ir pritaikytų modelį duomenims.
 predict_age <- function(data_test, idx, best_fit) {
@@ -235,16 +235,9 @@ predict_age <- function(data_test, idx, best_fit) {
   pred <- predict(best_fit, beta[,idx], s = "lambda.min")
   
   age_df <- data.frame(
-    real = colanns(data_test)$age,
-    id = colanns(data_test)$id
+    real = data_preped$age,
+    pred = exp(as.vector(pred))
   )
-  
-  # Kadangi atliekant prep_test (qc), galėjo būti atmesta dalis mėginių, atmetame dalį mėginių
-  # iš originalaus data_test (taip pat surikiuojame pagal tiriamuosius)
-  age_df <- age_df[age_df$id %in% rownames(pred), ]
-  
-  # Prijungiame prognozuotą amžių
-  age_df$pred <- (exp(as.vector(pred)))
   
   age_df
 }
@@ -254,11 +247,11 @@ age <- predict_age(data_test, idx, best_fit)
 
 # Apskaičiuojame MAE
 mean(abs(age$pred - age$real))
-# Gauname 5.352225
+# Gauname 5.124696
 
 # Apskaičiuojame koreliaciją
 cat("Pearson r:", round(cor(age$real, age$pred), 3), "\n")
-# Gauname 0.939
+# Gauname 0.947
 
 # Pavaizduosime realų vs. prognozuotą amžių
 
@@ -303,15 +296,3 @@ ggplot(age[101:157,]) +
   theme(
     plot.title = element_text(hjust = 0.5, size = 16),
     axis.text.x = element_blank())
-
-# Išvada kol kas:
-# Mokomas modelis jau labai tikslus pagal literatūra:
-# epigenetiniai laikrodziai kaip Horvath duoda MAE tarp 3-5 metu
-# koreliacija jei virs 0.95 - labai gera
-
-# Ka dar reikia padaryt:
-# Galima pabandyt dar pakoreguot modelio apmokyma su alpha
-# Siuo metu alfa parinktas random 0.5, bet derinant (mazinant ar aukstinant) butu galima
-# gal dar tikslesni rezultata gaut ir parodyt kaip buvo derinama
-# Pabaigti ir sukurti galutini modeli ant 5 duomenu rinkiniu, padaryti is jo funkcija ir istestuoti ant
-# palikto duomenu rinkinio quinn
